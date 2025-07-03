5794ce82271596433cbc219e691fc8a5de4f3fea
Harikrishnan <54831389+harikriz22@users.noreply.github.com>
2025-07-03 20:44:27 +0530

    Add files via upload

---
+++ data6.py
@@ -0,0 +1,166 @@
+# list
+
+# collection of data
+# []
+
+
+data = [11, 12, 13, 14, 15]
+
+# list are ordered
+# list can contain any arbitart type of data
+# list can be indexed
+# list can be nested
+# list are dynamic
+# list are mutable
+
+
+# list are ordered
+
+# a = [1,2,3,4]
+# b = [3,2,1,4]
+# print(a==b)
+
+# list can contain any arbitart type of data
+
+# data = [1,1.1,True,'hari',[3,4,5,6,],1,1,1,1,1,1]
+# print(data)
+
+
+# list can be indexed
+
+# a = [11,12,13,14,15,16,17,18,19.20,21,212]
+
+# print(a[::-1])
+
+# list can be nested
+# # ctrl+ /
+
+# a = [1,2,'hari',[11,12,['python'],14,15],5,6,'',8,True]
+# print(a[3][2][0])
+
+
+# list are dynamic
+# add - grow
+# remove - shrink
+
+# a=[11,12,13,14,15,1,22,23,24,25,26,27,27,8,78]
+# a[2:13]=[3,4]
+# print(a)
+
+
+# a = 'hari'
+# a[0] = 'l'
+# print(a)
+
+
+# string immutable unchangable
+# a = 'hari'
+
+# a = [11,12,13,14,15] #mutable 
+
+# a[0]='hari'
+
+# print(a)
+
+# inbuilt methods in list 
+
+# append(element) adds an element to the end of a list 
+
+# a = [1,2,3,4,5]
+# a.append('hari')
+# a.append(9)
+
+# print(a)
+
+# extend(itreable) adds an itreable to a list 
+# string 
+# list
+
+# a =[11,12,13,14,15]
+# a.extend([1,2,3,4,5])
+# a.extend('hari')
+
+# print(a)
+
+# insert(index,value)
+
+# a = [11,12,13,14,15,16]
+
+# a.insert(2,'hari')
+# print(a)
+
+
+
+# remove from a list 
+
+
+# remove(element) 
+
+# a= [1,2,3,1,2,3,3,2,1]
+# a.remove(3)
+# print(a)
+
+
+# pop
+
+# pop()
+# removes last element from a list 
+# a = [11,12,13,14,15,6]
+# a.pop(0)
+# print(a)
+
+# a.pop(0)
+
+# print(a)
+
+# a.pop(3)
+# print(a)
+
+# a.pop(3)
+# print(a)
+# pop(index)
+
+# append()
+# pop()
+
+# tuple 
+# collection of data 
+
+
+# a = (11,12,13,14,15)
+# a[0] = 100
+# print(a)
+# ordered
+# indexed
+# can have any arbitart elements 
+# can have duplicate elements
+# can be nested 
+# tuple are immutable 
+
+# tuple 
+# list 
+# string 
+print('string!!!!!!!!!!!!')
+a = 'hari'
+for i in range(0,len(a)):
+    print(i,a[i])
+b = [11,12,13,14,15]
+print('list!!!!!!!!!!')
+for i in range(len(b)):
+    print(i,b[i])
+print('tuple!!!!!!!!!!!')
+c = (21,22,23,24,25)
+for i in range(len(c)):
+    print(i,c[i])
+# for i in c:
+#     print(i)
+
+
+
+
+
+# itreables 
+# loop 
+# for loop
+
+
